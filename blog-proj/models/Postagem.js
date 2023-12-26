d229735c1ce9297d4ea9c443d6c09dcb1d264c33
jankenpowww <kusopunkk@gmail.com>
2023-12-25 22:02:25 -0300

    25/12/2023

---
+++ blog-proj/models/Postagem.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+const { Schema } = require("mongoose")
+
+const postagemSchema = new Schema({
+    titulo: {type: String, required: true},
+    descricao: {type: String, required: true},
+    conteudo: {type: String, required: true},
+    slug: {type: String, required: true},
+    data: {type: Date, default: Date.now()},
+
+    categoria: {
+        type: Schema.Types.ObjectId, //O valor de categoria será associada/relacionada ao id de um outro documento.
+        ref: "categorias", //O objeto 'ref' define o nome da coleção de referência no banco de dados à ser relacionada.
+        required: true
+    } 
+})
+
+const Postagem = mongoose.model("postagens", postagemSchema)
+
+module.exports = Postagem
