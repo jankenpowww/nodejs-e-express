1e2c6827848c5214a5487f566049cfb1890fe34e
jankenpowww <kusopunkk@gmail.com>
2024-01-04 16:57:56 -0300

    04/01/2024

--- blog-proj/app.js
+++ blog-proj/app.js
@@ -57,6 +57,7 @@
 
 //Models
     const Postagem = require("./models/Postagem")
+    const Categoria = require("./models/Categoria")
 
 //Importação de rotas
     const admin = require("./routes/admin") //Importando grupo de rotas relacionado ao administrador.
@@ -71,6 +72,8 @@
     app.get("/postagem/:slug", (req, res) => {
 
         Postagem.findOne({slug: req.params.slug}).populate("categoria", "nome").then((postagem) => {
+            
+            //Verifica se a postagem existe, uma vez que esse conteúdo pode ser alterado ou excluído do sistema.
             if (postagem) {
                 res.render("postagem/postagem", {postagem: postagem})
 
@@ -84,6 +87,44 @@
             res.redirect("/")
         })
         
+    })
+
+    app.get("/categorias", (req, res) => {
+        Categoria.find().then((categorias) => {
+            res.render("categoria/categoria", {categoria: categorias})
+
+        }).catch(() => {
+            req.flash("error_msg", "Não foi possível carregar alguns recursos. Tente novamente.")
+            res.redirect("/")
+
+        })
+    })
+
+    app.get("/categorias/:slug", (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).then((categoria) => {
+            
+            //Verificando se a categoria existe, uma vez que esse conteúdo pode ser alterado ou excluído do sistema.
+            if (categoria) {
+
+                Postagem.find({categoria: categoria._id}).sort({data: "desc"}).then((postagens) => {
+                    res.render("categoria/postagens-categoria", {postagem: postagens, categoria: categoria})
+
+                }).catch(() => {
+                    req.flash("error_msg", "Houve um erro ao listar os posts.")
+                    res.render("/")
+                    
+                })
+
+            } else {
+                req.flash("error_msg", "Esta categoria não existe.")
+                res.redirect("/")
+            }
+
+        }).catch(() => {
+            req.flash("error_msg", "Não foi possível carregar alguns recursos. Tente novamente.")
+            res.redirect("/")
+
+        })
     })
 
     app.use("/admin", admin) //Definindo um caminho e passando o grupo de rotas do administrador como relação.
